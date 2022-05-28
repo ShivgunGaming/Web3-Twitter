ea26fdbb5c6dd12aac2109976ed3c785b8fb57c1
Shivan <102505925+ShivgunGaming@users.noreply.github.com>
2022-05-28 11:18:40 +1000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="C73Wxw9gENbK72kmvTevQJ7MuxTrrdwZWFHYUjyC" serverUrl="https://ly4av7x3jj4j.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
